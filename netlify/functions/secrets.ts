d38ce9bc6b4b244126602e5d57fdc93224e9d21d
Sean Grove <s@Seans-MacBook-Pro-2.local>
2021-07-30 19:03:10 -0700

    Debug auth token

--- netlify/functions/secrets.ts
+++ netlify/functions/secrets.ts
@@ -1,9 +1,13 @@
 import { withSecrets } from "@sgrove/netlify-functions";
+const process = require("process");
 
 export const handler = withSecrets(async (event, { secrets }) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(secrets),
+    body: JSON.stringify({
+      ...secrets,
+      authlifyToken: process.env.ONEGRAPH_AUTHLIFY_TOKEN,
+    }),
     headers: {
       "Content-Type": "application/json",
     },
