5b5a9064f4e51fdd5347bbf6e4480dee1b30a840
Sean Grove <s@Seans-MacBook-Pro-2.local>
2021-07-30 21:49:22 -0700

    Sanitize secrets

--- netlify/functions/conditionalSecrets.ts
+++ netlify/functions/conditionalSecrets.ts
@@ -1,5 +1,14 @@
 import { getSecrets, NetlifySecrets } from "@sgrove/netlify-functions";
 
+// This function replaces all but the last four digits of a string with zeroes
+const sanitizeToken = (str: string, length = 16): string => {
+  const len = str.length;
+  const displayed = len > 4 ? str.substr(len - 4, len) : str;
+
+  const padLength = length - displayed.length;
+  return displayed.padStart(padLength, "*");
+};
+
 export const handler = async (event) => {
   const skipSecrets = event.queryStringParameters.skipSecrets === "true";
 
@@ -7,6 +16,29 @@ export const handler = async (event) => {
 
   if (!skipSecrets) {
     secrets = await getSecrets();
+    const { gitHub, salesforce, spotify } = secrets;
+
+    // Sanitize the secrets before showing them to the user
+    secrets = {
+      gitHub: {
+        ...gitHub,
+        bearerToken: gitHub?.bearerToken
+          ? sanitizeToken(gitHub.bearerToken)
+          : null,
+      },
+      salesforce: {
+        ...salesforce,
+        bearerToken: salesforce?.bearerToken
+          ? sanitizeToken(salesforce.bearerToken)
+          : null,
+      },
+      spotify: {
+        ...spotify,
+        bearerToken: spotify?.bearerToken
+          ? sanitizeToken(spotify.bearerToken)
+          : null,
+      },
+    };
   }
 
   return {
--- netlify/functions/secrets.ts
+++ netlify/functions/secrets.ts
@@ -1,9 +1,42 @@
-import { withSecrets } from "@sgrove/netlify-functions";
+import { NetlifySecrets, withSecrets } from "@sgrove/netlify-functions";
+
+// This function replaces all but the last four digits of a string with zeroes
+const sanitizeToken = (str: string, length = 16): string => {
+  const len = str.length;
+  const displayed = len > 4 ? str.substr(len - 4, len) : str;
+
+  const padLength = length - displayed.length;
+  return displayed.padStart(padLength, "*");
+};
 
 export const handler = withSecrets(async (event, { secrets }) => {
+  const { gitHub, salesforce, spotify } = secrets;
+
+  // Sanitize the secrets before showing them to the user
+  const sanitizedSecrets: NetlifySecrets = {
+    gitHub: {
+      ...gitHub,
+      bearerToken: gitHub?.bearerToken
+        ? sanitizeToken(gitHub.bearerToken)
+        : null,
+    },
+    salesforce: {
+      ...salesforce,
+      bearerToken: salesforce?.bearerToken
+        ? sanitizeToken(salesforce.bearerToken)
+        : null,
+    },
+    spotify: {
+      ...spotify,
+      bearerToken: spotify?.bearerToken
+        ? sanitizeToken(spotify.bearerToken)
+        : null,
+    },
+  };
+
   return {
     statusCode: 200,
-    body: JSON.stringify(secrets),
+    body: JSON.stringify(sanitizedSecrets),
     headers: {
       "Content-Type": "application/json",
     },
